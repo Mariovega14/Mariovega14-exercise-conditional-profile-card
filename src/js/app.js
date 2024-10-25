5eead59fe2753df19163df5d3ab5e0261c344bdc
Mario Vega <marioalbe123@gmail.com>
2024-10-25 19:50:11 +0000

    Primer Commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -29,18 +30,35 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  const {
+    name,
+    lastName,
+    twitter,
+    github,
+    linkedin,
+    instagram,
+    role,
+    country,
+    city,
+    socialMediaPosition
+  } = variables;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1> ${name != null ? name : "Nombre"} ${
+    lastName != null ? lastName : "Apellido"
+  }</h1>
+          <h2>${role == null ? "Trabajo" : role} </h2>
+          <h3>${city == null ? "Ciudad" : city}, ${
+    country == null ? "Pais" : country
+  }</h3>
+          <ul class="${socialMediaPosition}">
+            <li><a href="https://x.com/${twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
